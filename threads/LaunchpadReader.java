1e4fc9126c9fa7c49df796be6b7ccac9c7107c0e
Akash Chaturvedi <p.akashchaturvedi@gmail.com>
2016-04-26 19:53:49 -0500

    parsing GPS coordinates now

--- threads/LaunchpadReader.java
+++ threads/LaunchpadReader.java
@@ -22,22 +22,13 @@ public class LaunchpadReader extends Thread{
 				System.out.println("received coordinates..");
 				if(data != null){
 					System.out.println("launchpad wrote this: "+data);
-					//now broadcast this message i.e
-					//hand this message to ServerSocketWriter thread
-					/*
-					 * .
-					 * (do stuff here)
-					 */
+					
 					if((bq!=null) && (data != null)){
-						bq.put(data);
+						if(data.startsWith("$$GPGGA,")){
+							bq.put(data);
+						}
 					}
-				}
-				 /* .
-				 * ..
-				 * */
-				/*pass this message to SocketWriterThread, and he will write it to all the 
-				connected clinet's output streams
-				(may be I can use some Looper or something)*/			
+				}		
 			}
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
--- threads/ServerSocketWriter.java
+++ threads/ServerSocketWriter.java
@@ -27,27 +27,39 @@ public class ServerSocketWriter extends Thread{
 				 * do stuff here*/
 				String message = queue.take();
 				
+				//here we do the parsing
+				String[] splitted = message.split(",");
+				String time = splitted[1];
+				String lat = splitted[2];
+				String lng = splitted[4];
+				
+				
+				String lat1 = lat.substring(0, 2);
+				String lat2 = lat.substring(2);
+				
+				double actualLat = Double.parseDouble(lat1)+(Double.parseDouble(lat2)/60);
+				
+				String lng1 = lng.substring(0, 3);
+				String lng2 = lng.substring(3);
+				double actualLng = Double.parseDouble(lat1)+(Double.parseDouble(lat2)/60);
+				
+				String msgToClient = actualLat+",-"+actualLng;
 				//for now I am gonna go ahead and just print it
-				System.out.println("received this message on server: "+message);
+				//System.out.println("received this message on server: "+message);
 				
 				//later I am gonna write this message to all client's outputStreams (MainServer.clientList.getOutputStream())
                 //handle the data
 				/* 
 				 * */
-				System.out.println("the size of current client llist is: "+MainThread.clientList.size());
+				//System.out.println("the size of current client llist is: "+MainThread.clientList.size());
 				//a message here from any of the ServerSocketReader thread)
 				//2) write this message to outputstreams of all connected clients
+				
+				System.out.println("Writing this coordinates to the Android client: "+ msgToClient);
 				for(Client client: MainThread.clientList){
-					client.getOutputStream().writeBytes(message+"\n");
-					Thread.sleep(500);
-				}
-				
-				
-				/*pass this message to SocketriterThread, and he will write it to all the 
-				connected clinet's output streams
-				*/
-				
-			
+					client.getOutputStream().writeBytes(msgToClient+"\n");
+					//Thread.sleep(10);
+				}			
 			}
 		} catch (IOException | InterruptedException e) {
 			// TODO Auto-generated catch block
